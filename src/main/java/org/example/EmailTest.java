0abd2600aeda13b74462871f2e63b44ed1a1b8b8
Anupam Ahi <anupam.gollapalli@gmail.com>
2025-01-26 10:37:20 -0800

    Removed the emailcount method and fixed the other code

--- src/main/java/org/example/EmailTest.java
+++ src/main/java/org/example/EmailTest.java
@@ -3,6 +3,7 @@ import jakarta.mail.*;
 import jakarta.mail.search.BodyTerm;
 import jakarta.mail.search.SearchTerm;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -13,33 +14,18 @@ public class EmailTest {
         EmailTest.user = user;
         EmailTest.password = password;
     }
-    static Store establishConnection() throws MessagingException{
+    static Store establishConnection(String username, String password) throws MessagingException{
         Properties props = System.getProperties();
         props.setProperty("mail.store.protocol", "imaps");
         Session session = Session.getDefaultInstance(props, null);
         Store store = session.getStore("imaps");
-        store.connect("imap.gmail.com", user, password);
+        store.connect("imap.gmail.com", username, password);
         return store;
     }
-    static double emailCount(Store store, String folderName) throws MessagingException{
-        Folder inbox = store.getFolder(folderName);
+    static Message[] searchEmails(Store store, String keyword, String destinationFolder) throws MessagingException, IOException{
+        Folder inbox = store.getFolder(destinationFolder);
         inbox.open(Folder.READ_WRITE);
-        inbox.close(true);
-        return inbox.getMessageCount();
-    }
-    static void readEmail(Store store) throws MessagingException, IOException {
-        Folder inbox = store.getFolder("inbox");
-        inbox.open(Folder.READ_WRITE);
-        Message[] messages = inbox.getMessages();
-        if(messages.length > 0){
-            Message message = messages[0];
-        }
-        inbox.close(true);
-    }
-    static void searchEmails(Store store, String keyword, String destinationFolder) throws MessagingException, IOException{
-        Folder inbox = store.getFolder("inbox");
-        inbox.open(Folder.READ_WRITE);
-        SearchTerm emailTerm = new BodyTerm(keyword);
+        SearchTerm emailTerm  = new BodyTerm(keyword);
         Message[] messages = inbox.search(emailTerm);
         Arrays.sort(messages, (m1, m2) -> {
             try{
@@ -48,18 +34,18 @@ public class EmailTest {
                 throw new RuntimeException(e);
             }
         });
-        Message[] getFirstFiveEmails = Arrays.copyOfRange(messages, 0, 10);
-        for(Message message: getFirstFiveEmails){
+        for(Message message: messages){
             moveToFolder(store, message, destinationFolder);
         }
         inbox.close(true);
+        return messages;
     }
     static void moveToFolder(Store store, Message message, String folderName) throws MessagingException{
         Folder destinationFolder = store.getFolder(folderName);
         if(!destinationFolder.exists()) destinationFolder.create(Folder.HOLDS_MESSAGES);
         Message[] messagesToMove = new Message[] {message};
         message.getFolder().copyMessages(messagesToMove, destinationFolder);
-        message.setFlag(Flags.Flag.DELETED, true);
+//        message.setFlag(Flags.Flag.DELETED, true);
     }
     static void deleteEmail(Store store, String folderName) throws MessagingException{
         Folder folder = store.getFolder(folderName);
@@ -72,12 +58,26 @@ public class EmailTest {
         for(Message message: messages) message.setFlag(Flags.Flag.DELETED, true);
         folder.close(true);
     }
+
     public static void main(String[] args) throws MessagingException {
-        Store store = establishConnection();
-//        emailCount(store);
+        Properties props = new Properties();
+        try(InputStream input = EmailTest.class.getClassLoader().getResourceAsStream("config.properties")){
+            if(input != null){
+                props.load(input);
+            }
+            else{
+                System.out.println("Unable to load config.properties");
+                return;
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+            return;
+        }
+        String username = props.getProperty("username");
+        String appPassword = props.getProperty("appPassword");
+        Store store = establishConnection(username, appPassword);
         long startTime = System.currentTimeMillis();
         try{
-//            readEmail(store);
             searchEmails(store, "Unfortunately we will not be moving", "Rejections");
             deleteEmail(store, "Rejections");
             searchEmails(store, "We have received your application", "Applications");
@@ -87,6 +87,6 @@ public class EmailTest {
         }
         long endTime = System.currentTimeMillis();
         long execTime = endTime - startTime;
-        System.out.println("Time taken for sorting the first 10 emails of applications and rejections is: " + execTime);
+        System.out.println("Time taken for execution for processing the emails: " + execTime);
     }
 }
